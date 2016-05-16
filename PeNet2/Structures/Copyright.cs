23ac5715657d47fd8bf1790ecbcdb987b0d23696
secana <stefan.hausotte@gmx.de>
2016-05-16 22:35:29 +0200

    Added copyright and debug structure.

---
+++ PeNet2/Structures/Copyright.cs
@@ -0,0 +1,71 @@
+﻿/***********************************************************************
+Copyright 2016 Stefan Hausotte
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*************************************************************************/
+
+using System;
+using System.Text;
+
+namespace PeNet.Structures
+{
+    /// <summary>
+    /// The copyright ASCII (not 0-terminated) string of the PE file
+    /// if any is given.
+    /// </summary>
+    public class Copyright
+    {
+        private readonly byte[] _buff;
+        private readonly uint _offset;
+
+        /// <summary>
+        /// Create a new copyright object.
+        /// </summary>
+        /// <param name="buff">PE binary as byte array.</param>
+        /// <param name="offset">Offset to the copyright string in the binary.</param>
+        /// <param name="size">Size of the copyright string.</param>
+        public Copyright(byte[] buff, uint offset, uint size)
+        {
+            _buff = buff;
+            _offset = offset;
+
+            CopyrightString = ParseCopyrightString(buff, offset, size);
+        }
+
+        private string ParseCopyrightString(byte[] buff, uint offset, uint size)
+        {
+            var array = new byte[size];
+            Array.Copy(buff, offset, array, 0, size);
+            return Encoding.ASCII.GetString(array);
+        }
+
+        /// <summary>
+        /// The copyright string.
+        /// </summary>
+        public string CopyrightString { get; private set; }
+
+
+        /// <summary>
+        ///     Convert all object properties to strings.
+        /// </summary>
+        /// <returns>String representation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder("Copyright\n");
+            sb.Append(Utility.PropertiesToString(this, "{0,-10}:\t{1,10:X}\n"));
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ PeNet2/Structures/IMAGE_DEBUG_DIRECTORY.cs
@@ -0,0 +1,129 @@
+﻿/***********************************************************************
+Copyright 2016 Stefan Hausotte
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*************************************************************************/
+
+using System.Text;
+
+namespace PeNet.Structures
+{
+    /// <summary>
+    /// The IMAGE_DEBUG_DIRECTORY hold debug information
+    /// about the PE file.
+    /// </summary>
+    public class IMAGE_DEBUG_DIRECTORY
+    {
+        private readonly byte[] _buff;
+        private readonly uint _offset;
+
+        /// <summary>
+        /// Create a new IMAGE_DEBUG_DIRECTORY object.
+        /// </summary>
+        /// <param name="buff">PE binary as byte array.</param>
+        /// <param name="offset">Offset to the debug struct in the binary.</param>
+        public IMAGE_DEBUG_DIRECTORY(byte[] buff, uint offset)
+        {
+            _buff = buff;
+            _offset = offset;
+        }
+
+        /// <summary>
+        ///     Characteristics of the debug information.
+        /// </summary>
+        public uint Characteristics
+        {
+            get { return Utility.BytesToUInt32(_buff, _offset); }
+            set { Utility.SetUInt32(value, _offset, _buff); }
+        }
+
+        /// <summary>
+        ///    Time and date stamp
+        /// </summary>
+        public uint TimeDateStamp
+        {
+            get { return Utility.BytesToUInt32(_buff, _offset + 0x4); }
+            set { Utility.SetUInt32(value, _offset + 0x4, _buff); }
+        }
+
+        /// <summary>
+        /// Major Version.
+        /// </summary>
+        public ushort MajorVersion
+        {
+            get { return Utility.BytesToUInt16(_buff, _offset + 0x8); }
+            set { Utility.SetUInt16(value, _offset + 0x8, _buff); }
+        }
+
+        /// <summary>
+        /// Minor Version.
+        /// </summary>
+        public ushort MinorVersion
+        {
+            get { return Utility.BytesToUInt16(_buff, _offset + 0xa); }
+            set { Utility.SetUInt16(value, _offset + 0xa, _buff); }
+        }
+
+        /// <summary>
+        ///   Type
+        ///   1: Coff
+        ///   2: CV-PDB
+        ///   9: Borland
+        /// </summary>
+        public uint Type
+        {
+            get { return Utility.BytesToUInt32(_buff, _offset + 0xc); }
+            set { Utility.SetUInt32(value, _offset + 0xc, _buff); }
+        }
+
+        /// <summary>
+        ///   Size of data.
+        /// </summary>
+        public uint SizeOfData
+        {
+            get { return Utility.BytesToUInt32(_buff, _offset + 0x10); }
+            set { Utility.SetUInt32(value, _offset + 0x10, _buff); }
+        }
+
+        /// <summary>
+        ///    Address of raw data.
+        /// </summary>
+        public uint AddressOfRawData
+        {
+            get { return Utility.BytesToUInt32(_buff, _offset + 0x14); }
+            set { Utility.SetUInt32(value, _offset + 0x14, _buff); }
+        }
+
+        /// <summary>
+        ///    Pointer to raw data.
+        /// </summary>
+        public uint PointerToRawData
+        {
+            get { return Utility.BytesToUInt32(_buff, _offset + 0x18); }
+            set { Utility.SetUInt32(value, _offset + 0x18, _buff); }
+        }
+
+        /// <summary>
+        ///     Convert all object properties to strings.
+        /// </summary>
+        /// <returns>String representation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder("IMAGE_DEBUG_DIRECTORY\n");
+            sb.Append(Utility.PropertiesToString(this, "{0,-10}:\t{1,10:X}\n"));
+
+            return sb.ToString();
+        }
+    }
+}
