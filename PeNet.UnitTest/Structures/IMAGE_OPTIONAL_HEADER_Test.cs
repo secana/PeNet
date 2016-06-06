5a6ffa0d61870d04ad72473675ca078d1f1394d6
secana <stefan.hausotte@gmx.de>
2016-06-06 22:05:46 +0200

    Added test for debug directory

--- PeNet.UnitTest/Structures/IMAGE_OPTIONAL_HEADER_Test.cs
+++ PeNet.UnitTest/Structures/IMAGE_OPTIONAL_HEADER_Test.cs
@@ -1,4 +1,20 @@
-﻿using Castle.Core.Internal;
+﻿/***********************************************************************
+Copyright 2016 Stefan Hausotte
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*************************************************************************/
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PeNet.Structures;
 
@@ -7,6 +23,7 @@ namespace PeNet.UnitTest.Structures
     [TestClass]
     public class IMAGE_OPTIONAL_HEADER_Test
     {
+        #region Optional Header 64 Bit
         private readonly byte[] _rawImageOptionalHeader64Bit =
         {
             0xff, // Crap for offset
@@ -316,7 +333,9 @@ namespace PeNet.UnitTest.Structures
             0xcc,
             0x44
         };
+        #endregion
 
+        #region Optional Header 32 Bit
         private readonly byte[] _rawImageOptionalHeader32Bit =
         {
             0xff, // Crap for offset
@@ -610,6 +629,7 @@ namespace PeNet.UnitTest.Structures
             0xcc,
             0x44
         };
+#endregion
 
         [TestMethod]
         public void Constructor64BitWorks_Test()
