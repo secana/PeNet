93553b01cc0d121394464a029c37a0d603dc23f6
secana <stefan.hausotte@gmx.de>
2016-06-14 22:42:29 +0200

    Added unit test for IMAGE_IMPORT_BY_NAME

---
+++ PeNet.UnitTest/Structures/IMAGE_IMPORT_BY_NAME_Test.cs
@@ -0,0 +1,57 @@
+﻿/***********************************************************************
+Copyright 2016 Stefan Hausotte
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*************************************************************************/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PeNet.Structures;
+
+namespace PeNet.UnitTest.Structures
+{
+    [TestClass]
+    public class IMAGE_IMPORT_BY_NAME_Test
+    {
+        private readonly byte[] _rawImportByName =
+        {
+            0xff, // Junk
+            0xff,
+
+            0x00, // Hint
+            0x11,
+
+            // Name
+            72,  // H
+            101, // e
+            108, // l
+            108, // l
+            111, // o
+            32,  // ' '
+            87,  // W
+            111, // o
+            114, // r
+            108, // l
+            100, // d
+            00
+        };
+
+        [TestMethod]
+        public void ImageImportByNameConstructorWorks_Test()
+        {
+            var importByName = new IMAGE_IMPORT_BY_NAME(_rawImportByName, 2);
+            Assert.AreEqual((ushort) 0x1100, importByName.Hint);
+            Assert.AreEqual("Hello World", importByName.Name);
+        }
+    }
+}
