d9e5056ad8aa1016630b1f3ba79a0ff4a4dcedcb
Stefan Hausotte <stefan.hausotte@gdata.de>
2017-12-08 12:26:10 +0100

    Added unit test for authenticode

---
+++ test/PeNet.Test/Binaries/Authenticode_Test.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace PeNet.Test.Binaries
+{
+    public class Authenticode_Test
+    {
+        [Fact]
+        public void IsSignatureValid_SigendBinary_ReturnsTrue()
+        {
+            var _peFile = new PeFile(@"../../../Binaries/firefox.exe");
+            Assert.True(_peFile.IsSignatureValid);
+        }
+
+         
+        [Fact]
+        public void IsSignatureValid_UnsigendBinary_ReturnsFalse()
+        {
+            var _peFile = new PeFile(@"../../../Binaries/TLSCallback_x86.exe");
+            Assert.False(_peFile.IsSignatureValid);
+        }
+    }
+}
