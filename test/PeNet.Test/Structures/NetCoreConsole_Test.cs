aed25c07dec3bd720b5f830bca33c0d880a292e9
Stefan Hausotte <stefan.hausotte@gdata.de>
2019-02-01 16:01:17 +0100

    Add more test for .Net header parsing (currently tests are broken)

--- test/PeNet.Test/Structures/NetCoreConsole_Test.cs
+++ test/PeNet.Test/Structures/NetCoreConsole_Test.cs
@@ -3,9 +3,9 @@ using Xunit;
 
 namespace PeNet.Test.Structures
 {
-    public class NetFrameworkConsole_Test
+    public class NetCoreConsole_Test
     {
-        private readonly PeFile _peFile = new PeFile(@"./Binaries/NetFrameworkConsole.exe");
+        private readonly PeFile _peFile = new PeFile(@"./Binaries/NetCoreConsole.dll");
 
         [Fact]
         public void DataDirectory_COMDescripterSet()
@@ -30,11 +30,11 @@ namespace PeNet.Test.Structures
             Assert.Equal(0x00000048u, netDirectory.cb);
             Assert.Equal(0x0002u, netDirectory.MajorRuntimeVersion);
             Assert.Equal(0x0005u, netDirectory.MinorRuntimeVersion);
-            Assert.Equal(0x000020ACu, netDirectory.MetaData.VirtualAddress);
-            Assert.Equal(0x00000728u, netDirectory.MetaData.Size);
-            Assert.Equal(0x00020003u, netDirectory.Flags);
-            Assert.Equal(0x06000001u, netDirectory.EntryPointToken);
-            Assert.Equal(0x06000001u, netDirectory.EntryPointRVA);
+            Assert.Equal(0x00003A38u, netDirectory.MetaData.VirtualAddress);
+            Assert.Equal(0x000056F8u, netDirectory.MetaData.Size);
+            Assert.Equal(0x00000001u, netDirectory.Flags);
+            Assert.Equal(0x0600000Cu, netDirectory.EntryPointToken);
+            Assert.Equal(0x0600000Cu, netDirectory.EntryPointRVA);
             Assert.Equal(0x00000000u, netDirectory.Resources.VirtualAddress);
             Assert.Equal(0x00000000u, netDirectory.Resources.Size);
             Assert.Equal(0x00000000u, netDirectory.StrongNameSignature.VirtualAddress);
@@ -65,7 +65,7 @@ namespace PeNet.Test.Structures
         }
 
         [Fact]
-        public void MetaDataStreamTables_ParseCorrectValues()
+        public void MetaDataStreamTablesHeader_ParseCorrectValues()
         {
             var tablesHeader = _peFile.MetaDataStreamTablesHeader;
 
@@ -73,7 +73,7 @@ namespace PeNet.Test.Structures
             Assert.Equal(0x02u, tablesHeader.MajorVersion);
             Assert.Equal(0x00u, tablesHeader.MinorVersion);
             Assert.Equal(0x01u, tablesHeader.Reserved2);
-            Assert.Equal(0x0000000908021547u, tablesHeader.Valid);
+            Assert.Equal(0x00001E0909A21F57u, tablesHeader.Valid);
             Assert.Equal(0x000016003301FA00u, tablesHeader.MaskSorted);
         }
 
@@ -82,53 +82,12 @@ namespace PeNet.Test.Structures
         {
             var strings = _peFile.MetaDataStreamString.Strings;
 
-            Assert.Equal(46, strings.Count);
-            Assert.Equal("IEnumerable`1", strings[0]);
-            Assert.Equal("IEnumerator`1", strings[1]);
-            Assert.Equal("<Module>", strings[2]);
-            Assert.Equal("System.IO", strings[3]);
-            Assert.Equal("mscorlib", strings[4]);
-            Assert.Equal("System.Collections.Generic", strings[5]);
-            Assert.Equal("IDisposable", strings[6]);
-            Assert.Equal("NetFrameworkConsole", strings[7]);
-            Assert.Equal("WriteLine", strings[8]);
-            Assert.Equal("Dispose", strings[9]);
-            Assert.Equal("GuidAttribute", strings[10]);
-            Assert.Equal("DebuggableAttribute", strings[11]);
-            Assert.Equal("ComVisibleAttribute", strings[12]);
-            Assert.Equal("AssemblyTitleAttribute", strings[13]);
-            Assert.Equal("AssemblyTrademarkAttribute", strings[14]);
-            Assert.Equal("TargetFrameworkAttribute", strings[15]);
-            Assert.Equal("AssemblyFileVersionAttribute", strings[16]);
-            Assert.Equal("AssemblyConfigurationAttribute", strings[17]);
-            Assert.Equal("AssemblyDescriptionAttribute", strings[18]);
-            Assert.Equal("CompilationRelaxationsAttribute", strings[19]);
-            Assert.Equal("AssemblyProductAttribute", strings[20]);
-            Assert.Equal("AssemblyCopyrightAttribute", strings[21]);
-            Assert.Equal("AssemblyCompanyAttribute", strings[22]);
-            Assert.Equal("RuntimeCompatibilityAttribute", strings[23]);
-            Assert.Equal("NetFrameworkConsole.exe", strings[24]);
-            Assert.Equal("System.Runtime.Versioning", strings[25]);
-            Assert.Equal("Program", strings[26]);
-            Assert.Equal("System", strings[27]);
-            Assert.Equal("Main", strings[28]);
-            Assert.Equal("System.Reflection", strings[29]);
-            Assert.Equal("ConsoleKeyInfo", strings[30]);
-            Assert.Equal("IEnumerator", strings[31]);
-            Assert.Equal("GetEnumerator", strings[32]);
-            Assert.Equal(".ctor", strings[33]);
-            Assert.Equal("System.Diagnostics", strings[34]);
-            Assert.Equal("System.Runtime.InteropServices", strings[35]);
-            Assert.Equal("System.Runtime.CompilerServices", strings[36]);
-            Assert.Equal("DebuggingModes", strings[37]);
-            Assert.Equal("EnumerateDirectories", strings[38]);
-            Assert.Equal("args", strings[39]);
-            Assert.Equal("System.Collections", strings[40]);
-            Assert.Equal("Object", strings[41]);
-            Assert.Equal("get_Current", strings[42]);
-            Assert.Equal("MoveNext", strings[43]);
-            Assert.Equal("ReadKey", strings[44]);
-            Assert.Equal("Directory", strings[45]);
+            Assert.Equal(465, strings.Count);
+            Assert.Equal("<>9__0_0", strings[0]);
+            // ... More strings ...
+            Assert.Equal("get_GdcLoudCheck", strings[228]);
+            // ... More strings ...
+            Assert.Equal("IsNullOrEmpty", strings[464]);
         }
 
         [Fact]
@@ -136,8 +95,10 @@ namespace PeNet.Test.Structures
         {
             var us = _peFile.MetaDataStreamUS;
 
-            Assert.Single(us.UserStrings);
-            Assert.Equal(@"C:\", us.UserStrings[0]);
+            Assert.Equal(51, us.UserStrings.Count);
+            Assert.Equal("CandCMetaInformationService", us.UserStrings[0]);
+            Assert.Equal("Metainformation Proto3 Request for {0}.", us.UserStrings[44]);
+            Assert.Equal("ServiceEndpoints:Redis:CacheExpire:Minutes", us.UserStrings[50]);
         }
 
         [Fact]
@@ -145,7 +106,7 @@ namespace PeNet.Test.Structures
         {
             var guid = _peFile.MetaDataStreamGUID;
             Assert.Single(guid.GuidsAndIndices);
-            Assert.Equal(new Guid("5250e853-c17a-4e76-adb3-0a716ec8af5d"), guid.Guids[0]);
+            Assert.Equal(new Guid("42f102e3-93ed-453b-bb95-d932c33520d4"), guid.Guids[0]);
         }
 
         [Fact]
@@ -154,10 +115,10 @@ namespace PeNet.Test.Structures
             var blob = _peFile.MetaDataStreamBlob;
 
             // Just test a few values instead of the whole blob
-            Assert.Equal(0x150, blob.Length);
+            Assert.Equal(0xC0C, blob.Length);
             Assert.Equal(0x00, blob[0]);
-            Assert.Equal(0x4E, blob[0x97]);
-            Assert.Equal(0x00, blob[0x14F]);
+            Assert.Equal(0x81, blob[0x400]);
+            Assert.Equal(0x70, blob[0xB70]);
         }
 
         [Fact]
@@ -166,16 +127,18 @@ namespace PeNet.Test.Structures
             var metaDataTablesHdr = _peFile.MetaDataStreamTablesHeader;
 
             Assert.Equal("Module", metaDataTablesHdr.TableDefinitions[0].Name);
-            Assert.Equal(1U, metaDataTablesHdr.TableDefinitions[0].RowCount);
-            Assert.Equal(0U, metaDataTablesHdr.TableDefinitions[0].Offset);
-            Assert.Equal(10U, metaDataTablesHdr.TableDefinitions[0].BytesPerRow);
+            Assert.Equal(0x1U, metaDataTablesHdr.TableDefinitions[0].RowCount);
+            Assert.Equal(0x00U, metaDataTablesHdr.TableDefinitions[0].Offset);
+            Assert.Equal(0x0AU, metaDataTablesHdr.TableDefinitions[0].BytesPerRow);
 
             // ... More tables ...
           
             Assert.Equal("AssemblyRef", metaDataTablesHdr.TableDefinitions[35].Name);
-            Assert.Equal(1U, metaDataTablesHdr.TableDefinitions[35].RowCount);
-            Assert.Equal(454U, metaDataTablesHdr.TableDefinitions[35].Offset);
+            Assert.Equal(30U, metaDataTablesHdr.TableDefinitions[35].RowCount);
+            Assert.Equal(0x1B50U, metaDataTablesHdr.TableDefinitions[35].Offset);
             Assert.Equal(20U, metaDataTablesHdr.TableDefinitions[35].BytesPerRow);
+
+            // ... More tables ...
         }
 
         [Fact]
