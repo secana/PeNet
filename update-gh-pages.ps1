406b7c6efe3f5725f0f1bac3344a50743ac1d69b
Stefan Hausotte <stefan.hausotte@gdata.de>
2019-02-08 09:26:33 +0100

    small improvement

--- update-gh-pages.ps1
+++ update-gh-pages.ps1
@@ -3,12 +3,13 @@ Param(
     [string]$token
 )
 
+$url = "https://azure-bot:"+$token+"@github.com/secana/PeNet.git"
 New-Item -Type Directory tmp
-git clone https://azure-bot:$token@github.com/secana/PeNet.git .\tmp\PeNet --single-branch --branch gh-pages
+git clone $url .\tmp\PeNet --single-branch --branch gh-pages
 Copy-Item -Recurse -Force .\docs\* .\tmp\PeNet\
 Set-Location .\tmp\PeNet
 git remote rm origin
-git remote add origin https://azure-bot:$token@github.com/secana/PeNet.git
+git remote add origin $url
 git config user.email azure-bot@penet.org
 git config user.name "Azure Bot"
 git add .
