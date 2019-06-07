0e60249007cb914415563fa9c8ad74ae4bf80ea8
Stefan Hausotte <stefan.hausotte@outlook.com>
2019-06-07 09:46:49 +0200

    Fix #81 -> Typo in NetRunFroMSwap

--- src/PeNet/FileCharacteristics.cs
+++ src/PeNet/FileCharacteristics.cs
@@ -48,7 +48,7 @@ namespace PeNet
                 RemovableRunFromSwap = true;
 
             if ((characteristics & (ushort) Constants.FileHeaderCharacteristics.IMAGE_FILE_NET_RUN_FROM_SWAP) > 0)
-                NetRunFroMSwap = true;
+                NetRunFromSwap = true;
 
             if ((characteristics & (ushort) Constants.FileHeaderCharacteristics.IMAGE_FILE_SYSTEM) > 0)
                 System = true;
@@ -116,7 +116,7 @@ namespace PeNet
         /// <summary>
         ///     If the image is on the network, copy and run it from the swap file.
         /// </summary>
-        public bool NetRunFroMSwap { get; private set; }
+        public bool NetRunFromSwap { get; private set; }
 
         /// <summary>
         ///     The image is a system file.
