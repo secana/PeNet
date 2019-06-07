b70589d21a6482c345547f1c56836410c99aac20
Stefan Hausotte <stefan.hausotte@outlook.com>
2019-06-07 13:39:44 +0200

    Add test for "ToString" override

--- src/PeNet/Structures/AbstractStructure.cs
+++ src/PeNet/Structures/AbstractStructure.cs
@@ -1,5 +1,7 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
 
---
+++ test/PeNet.Test/Structures/AbstractStructure_Test.cs
@@ -0,0 +1,47 @@
+﻿using PeNet.Structures;
+using Xunit;
+
+namespace PeNet.Test.Structures
+{
+    public class AbstractStructure_Test
+    {
+        class SubStructure : AbstractStructure
+        {
+            public int SubInt { get; } = 11;
+            public int SubString { get; } = 12;
+            public SubStructure() 
+                : base(null, 0)
+            {
+            }
+        }
+        class Structure : AbstractStructure
+        {
+            public int Integer { get; } = 10;
+            public string String { get; } = "Hello";
+            public long Long { get; } = 20;
+            public string[] StringArray { get; } = new string[] { "Hello", "World" }; // Must not be in the results
+            public int[] NullArray { get; } = null; // Must not be in the results
+            public string NullString { get; } = null;
+            public SubStructure[] SubStructure { get; } = new SubStructure[] { new SubStructure(), new SubStructure() };
+
+            private string PrivString { get; } = "ShallNotSee"; // Must not be in the results
+            protected int ProtInt { get; } = -1;
+
+            public Structure() 
+                : base(null, 0)
+            {
+            }
+        }
+
+        [Fact]
+        public void ToString_GivenAnAbstractStructure_ReturnsStringRepresentation()
+        {
+            var structure = new Structure();
+            var expected = "Structure\nInteger: 10\nString: Hello\nLong: 20\nNullString: \nSubStructure\nSubInt: 11\nSubString: 12\nSubStructure\nSubInt: 11\nSubString: 12\n";
+
+            var actual = structure.ToString();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
