e8ea710ee9e8757f2c09227d72f75f2d9688afad
secana <stefan.hausotte@gmx.de>
2019-08-12 15:36:41 +0200

    Parse forwarded exports #90

--- src/PeNet/ExportFunction.cs
+++ src/PeNet/ExportFunction.cs
@@ -22,6 +22,21 @@ namespace PeNet
         }
 
         /// <summary>
+        ///     Create a new ExportFunction object.
+        /// </summary>
+        /// <param name="name">Name of the function.</param>
+        /// <param name="address">Address of function.</param>
+        /// <param name="ordinal">Ordinal of the function.</param>
+        /// <param name="forwardName">Name of the DLL and function this export forwards to.</param>
+        public ExportFunction(string name, uint address, ushort ordinal, string forwardName)
+        {
+            Name = name;
+            Address = address;
+            Ordinal = ordinal;
+            ForwardName = forwardName;
+        }
+
+        /// <summary>
         ///     Function name.
         /// </summary>
         public string Name { get; private set; }
@@ -35,5 +50,29 @@ namespace PeNet
         ///     Function Ordinal.
         /// </summary>
         public ushort Ordinal { get; }
+
+        /// <summary>
+        ///     Function name if the function is
+        ///     forwarded to another DLL.
+        ///     Format "DLLName.ExportName".
+        /// </summary>
+        public string ForwardName { get; }
+
+
+        /// <summary>
+        ///     True if the export has a name and is not forwarded.
+        /// </summary>
+        public bool HasName => !string.IsNullOrEmpty(Name);
+
+        /// <summary>
+        ///     True if the export has an ordinal.
+        /// </summary>
+        public bool HasOrdinal => Ordinal != 0;
+
+        /// <summary>
+        ///     True if the export is forwared and has 
+        ///     a ForwardName.
+        /// </summary>
+        public bool HasForwad => !string.IsNullOrEmpty(ForwardName);
     }
 }
--- test/PeNet.Test/PeFile_Test.cs
+++ test/PeNet.Test/PeFile_Test.cs
@@ -1,6 +1,7 @@
-﻿using Xunit;
+﻿using System.Linq;
+using Xunit;
 
-namespace PeNet.Test.Utilities
+namespace PeNet.Test.PeFile_Test
 {
     public class PeFile
     {
@@ -12,6 +13,16 @@ namespace PeNet.Test.Utilities
         public void IsPEFile_DifferentFiles_TrueOrFalse(string file, bool expected)
         {
             Assert.Equal(expected, PeNet.PeFile.IsPEFile(file));
+        }
+
+        [Fact]
+        public void ExportedFunctions_WithForwardedFunctions_ParsedFordwardedFunctions()
+        {
+            var peFile = new PeNet.PeFile(@"Binaries/win_test.dll");
+            var forwardExports = peFile.ExportedFunctions.Where(e => e.HasForwad).ToList();
+
+            Assert.Equal(180, forwardExports.Count);
+            Assert.Equal("NTDLL.RtlEnterCriticalSection", forwardExports.First(e => e.Name == "EnterCriticalSection").ForwardName);
         }
     }
 }
