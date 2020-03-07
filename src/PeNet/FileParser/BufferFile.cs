ae41df74c875d2eb52828aa953888048f469bb00
secana <stefan.hausotte@gmx.de>
2020-03-07 21:47:57 +0100

    Implement BufferFile

--- src/PeNet/FileParser/BufferFile.cs
+++ src/PeNet/FileParser/BufferFile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace PeNet.FileParser
 {
@@ -7,78 +8,110 @@ namespace PeNet.FileParser
     {
         private readonly byte[] _buff;
 
-        public long Length => throw new NotImplementedException();
+        public long Length => _buff.Length;
 
         public BufferFile(byte[] file) => (_buff) = (file);
 
         public string GetCString(long offset)
         {
-            throw new NotImplementedException();
+            var length = GetCStringLength(_buff, offset);
+            var tmp = new char[length];
+            for (long i = 0; i < length; i++)
+            {
+                tmp[i] = (char)_buff[offset + i];
+            }
+
+            return new string(tmp);
         }
 
-        public Span<byte> GetSpan(long offset, long length)
+        private int GetCStringLength(byte[] buff, long stringOffset)
         {
-            throw new NotImplementedException();
+            var offset = stringOffset;
+            var length = 0;
+            while (buff[offset] != 0x00)
+            {
+                length++;
+                offset++;
+            }
+            return length;
         }
+
+        public Span<byte> GetSpan(long offset, long length) 
+            => _buff.AsSpan((int) offset, (int) length);
 
         public string GetUnicodeString(long offset)
         {
-            throw new NotImplementedException();
+            var size = 0;
+            for (var i = 0; i < (_buff.Length - offset) - 1; i++)
+            {
+                if (_buff[offset + i] == 0 && _buff[(int)offset + (i + 1)] == 0)
+                {
+                    size = i + 1;
+                    break;
+                }
+            }
+
+            var bytes = new byte[size];
+
+            Array.Copy(_buff, (int)offset, bytes, 0, size);
+            return Encoding.Unicode.GetString(bytes);
         }
 
-        public byte ReadByte(long offset)
-        {
-            throw new NotImplementedException();
-        }
+        public byte ReadByte(long offset) => _buff[offset];
 
         public uint ReadUInt(long offset)
-        {
-            throw new NotImplementedException();
-        }
+            => BitConverter.ToUInt32(_buff, (int) offset);
 
         public ulong ReadULong(long offset)
-        {
-            throw new NotImplementedException();
-        }
+            => BitConverter.ToUInt64(_buff, (int) offset);
 
         public ushort ReadUShort(long offset)
-        {
-            throw new NotImplementedException();
-        }
+            => BitConverter.ToUInt16(_buff, (int) offset);
 
-        public Stream ToStream()
-        {
-            throw new NotImplementedException();
-        }
+        public Stream ToStream() => new MemoryStream(_buff);
 
         public void WriteByte(long offset, byte value)
         {
-            throw new NotImplementedException();
+            _buff[offset] = value;
         }
 
         public void WriteBytes(long offset, Span<byte> bytes)
         {
-            throw new NotImplementedException();
+            for(var i = (int) offset; i < bytes.Length; i++)
+            {
+                _buff[offset] = bytes[i];
+            }
         }
 
         public void WriteUInt(long offset, uint value)
         {
-            throw new NotImplementedException();
+            var x = BitConverter.GetBytes(value);
+            _buff[offset] = x[0];
+            _buff[offset + 1] = x[1];
+            _buff[offset + 2] = x[2];
+            _buff[offset + 3] = x[3];
         }
 
         public void WriteULong(long offset, ulong value)
         {
-            throw new NotImplementedException();
+            var x = BitConverter.GetBytes(value);
+            _buff[offset] = x[0];
+            _buff[offset + 1] = x[1];
+            _buff[offset + 2] = x[2];
+            _buff[offset + 3] = x[3];
+            _buff[offset + 4] = x[4];
+            _buff[offset + 5] = x[5];
+            _buff[offset + 6] = x[6];
+            _buff[offset + 7] = x[7];
         }
 
         public void WriteUShort(long offset, ushort value)
         {
-            throw new NotImplementedException();
+            var x = BitConverter.GetBytes(value);
+            _buff[offset] = x[0];
+            _buff[offset + 1] = x[1];
         }
 
-        public byte[] ToArray()
-        {
-            throw new NotImplementedException();
-        }
+        public byte[] ToArray() => _buff;
     }
 }
