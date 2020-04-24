ee67f544af3c27dd83eb762eddff4020fba26dde
secana <stefan.hausotte@gmx.de>
2020-04-24 13:23:38 +0200

    Add memory mapped file as a IRawFile method

---
+++ src/PeNet/FileParser/MMFile.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+
+namespace PeNet.FileParser
+{
+    public class MMFile : IRawFile
+    {
+        private const int MaxStackAlloc = 1024;
+        private MemoryMappedFile _mmf;
+        private MemoryMappedViewAccessor _va;
+        private MemoryMappedViewStream? _stream;
+        private FileInfo _fileInfo;
+
+        public MMFile(string file)
+        {
+            _mmf = MemoryMappedFile.CreateFromFile(file, FileMode.Open);
+            _va = _mmf.CreateViewAccessor();
+            _fileInfo = new FileInfo(file);
+            RemoveRangeNewFileName = _fileInfo.FullName;
+        }
+
+        public string RemoveRangeNewFileName
+        {
+            get; set;
+        }
+
+        public long Length
+            => _fileInfo.Length;
+
+        public Span<byte> AsSpan(long offset, long length)
+        {
+            var array = new byte[length];
+            _va.ReadArray(offset, array, 0, (int) length);
+
+            return array.AsSpan();
+        }
+
+        public void Dispose()
+        {
+            _va.Dispose();
+            _stream?.Dispose();
+            _mmf.Dispose();
+        }
+
+        public string ReadAsciiString(long offset)
+        {
+            static int GetCStringLength(MemoryMappedViewAccessor va, long stringOffset)
+            {
+                var currentOffset = stringOffset;
+                var currentLength = 0;
+                while (va.ReadByte(currentOffset) != 0x00)
+                {
+                    currentLength++;
+                    currentOffset++;
+                }
+                return currentLength;
+            }
+
+            var length = GetCStringLength(_va, offset);
+
+            var tmp = length > MaxStackAlloc
+                ? new char[length]
+                : stackalloc char[length];
+
+            for (var i = 0; i < length; i++)
+            {
+                tmp[i] = (char)_va.ReadByte(offset + i);
+            }
+
+            return new string(tmp);
+        }
+
+        public byte ReadByte(long offset)
+            => _va.ReadByte(offset);
+
+
+        public uint ReadUInt(long offset)
+            => _va.ReadUInt32(offset);
+
+        public ulong ReadULong(long offset)
+            => _va.ReadUInt64(offset);
+
+        public string ReadUnicodeString(long offset)
+        {
+            var size = 1;
+            for (var i = offset; i < Length - 1; i++)
+            {
+                if (_va.ReadByte(i) == 0 && _va.ReadByte(i + 1) == 0)
+                {
+                    break;
+                }
+                size++;
+            }
+            var bytes = new byte[size];
+
+            _va.ReadArray(offset, bytes, 0, size);
+            return Encoding.Unicode.GetString(bytes);
+        }
+
+        public ushort ReadUShort(long offset)
+            => _va.ReadUInt16(offset);
+
+        public void RemoveRange(long offset, long length)
+        {
+            var list = ToArray().ToList();
+            list.RemoveRange((int)offset, (int)length);
+
+            _va.Dispose();
+            _mmf.Dispose();
+            var file = RemoveRangeNewFileName;
+
+            File.WriteAllBytes(file, list.ToArray());
+
+            _mmf = MemoryMappedFile.CreateFromFile(file, FileMode.Open);
+            _va = _mmf.CreateViewAccessor();
+            _fileInfo = new FileInfo(file);
+        }
+
+        public byte[] ToArray()
+        {
+            var array = new byte[Length];
+            _va.ReadArray(0, array, 0, (int) Length);
+
+            return array;
+        }
+
+        public Stream ToStream()
+        {
+            if (_stream is null)
+                _stream = _mmf.CreateViewStream();
+
+            return _stream;
+        }
+
+
+        public void WriteByte(long offset, byte value)
+        {
+            var tmp = new byte[] { value };
+            _va.WriteArray(offset, tmp, 0, 1);
+
+        }
+
+        public void WriteBytes(long offset, Span<byte> bytes)
+            => _va.WriteArray(offset, bytes.ToArray(), 0, bytes.Length);
+
+        public void WriteUInt(long offset, uint value)
+        {
+            var tmp = BitConverter.GetBytes(value);
+            _va.WriteArray(offset, tmp, 0, 4);
+        }
+
+        public void WriteULong(long offset, ulong value)
+        {
+            var tmp = BitConverter.GetBytes(value);
+            _va.WriteArray(offset, tmp, 0, 8);
+        }
+
+        public void WriteUShort(long offset, ushort value)
+        {
+            var tmp = BitConverter.GetBytes(value);
+            _va.WriteArray(offset, tmp, 0, 2);
+        }
+    }
+}
---
+++ test/PeNet.Test/FileParser/MMFileTest.cs
@@ -0,0 +1,179 @@
+﻿using PeNet.FileParser;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using Xunit;
+
+namespace PeNet.Test.FileParser
+{
+    public class MMFileTest
+    {
+        [Fact]
+        public void Length_ReturnsCorrectLength()
+        {
+            using var mmf = new MMFile(@"Binaries/firefox_x86.exe");
+
+            Assert.Equal(411_088, mmf.Length);
+        }
+
+        [Fact]
+        public void AsSpan_ReturnsSpan()
+        {
+            using var mmf = new MMFile(@"Binaries/mmf_test.bin");
+
+            var s = mmf.AsSpan(0x16, 3);
+
+            Assert.Equal(3, s.Length);
+            Assert.Equal(0x11, s[0]);
+            Assert.Equal(0x22, s[1]);
+            Assert.Equal(0x33, s[2]);
+        }
+
+        [Fact]
+        public void ReadAsciiString_ReturnsString()
+        {
+            using var mmf = new MMFile(@"Binaries/mmf_test.bin");
+
+            var actual = mmf.ReadAsciiString(0x26);
+
+            Assert.Equal("hello world.", actual);
+        }
+
+        [Fact]
+        public void ReadByte_ReturnsByte()
+        {
+            using var mmf = new MMFile(@"Binaries/mmf_test.bin");
+
+            var actual = mmf.ReadByte(0x2A);
+
+            Assert.Equal(0x6F, actual);
+        }
+
+        [Fact]
+        public void ReadUInt_ReturnsUInt()
+        {
+            using var mmf = new MMFile(@"Binaries/mmf_test.bin");
+
+            var actual = mmf.ReadUInt(0x16);
+
+            Assert.Equal(0x44332211u, actual);
+        }
+
+        [Fact]
+        public void ReadULong_ReturnsULong()
+        {
+            using var mmf = new MMFile(@"Binaries/mmf_test.bin");
+
+            var actual = mmf.ReadULong(0x16);
+
+            Assert.Equal(0x8877665544332211u, actual);
+        }
+
+        [Fact]
+        public void ReadUnicodeString_ReturnsString()
+        {
+            using var mmf = new MMFile(@"Binaries/mmf_test.bin");
+
+            var actual = mmf.ReadUnicodeString(0x34);
+
+            Assert.Equal("hello", actual);
+        }
+
+        [Fact]
+        public void ReadUShort_ReturnsUShort()
+        {
+            using var mmf = new MMFile(@"Binaries/mmf_test.bin");
+
+            var actual = mmf.ReadUShort(0x16);
+
+            Assert.Equal((ushort) 0x2211, actual);
+        }
+
+        [Fact]
+        public void RemoveRange_RemovesRange()
+        {
+            using var mmf = new MMFile(@"Binaries/mmf_test.bin")
+            {
+                RemoveRangeNewFileName = "mmf_test_removerange.bin"
+            };
+
+            mmf.RemoveRange(2, 0x3E);
+
+            Assert.Equal(4, mmf.Length);
+            Assert.Equal(0x22116874u, mmf.ReadUInt(0));
+        }
+
+        [Fact]
+        public void ToArray_ReturnsArray()
+        {
+            using var mmf = new MMFile(@"Binaries/mmf_test.bin");
+
+            var actual = mmf.ToArray();
+
+            Assert.Equal(0x42, actual.Length);
+        }
+
+        [Fact]
+        public void ToStream_ReturnStream()
+        {
+            using var mmf = new MMFile(@"Binaries/mmf_test.bin");
+
+            Assert.NotNull(mmf.ToStream());
+            Assert.IsType<MemoryMappedViewStream>(mmf.ToStream());
+        }
+
+        [Fact]
+        public void WriteByte_WritesByte()
+        {
+            File.Copy(@"Binaries/mmf_test.bin", @"Binaries/mmf_test_writebyte.bin", true);
+            using var mmf = new MMFile(@"Binaries/mmf_test_writebyte.bin");
+
+            mmf.WriteByte(0x15, 0xaf);
+
+            Assert.Equal((byte) 0xaf, mmf.ReadByte(0x15));
+        }
+
+        [Fact]
+        public void WriteBytes_WritesBytes()
+        {
+            File.Copy(@"Binaries/mmf_test.bin", @"Binaries/mmf_test_writebytes.bin", true);
+            using var mmf = new MMFile(@"Binaries/mmf_test_writebytes.bin");
+
+            mmf.WriteBytes(0x15, new byte[] { 0xaf, 0xbf });
+
+            Assert.Equal(0xbfaf, mmf.ReadUShort(0x15));
+        }
+
+        [Fact]
+        public void WriteUInt_WritesUInt()
+        {
+            File.Copy(@"Binaries/mmf_test.bin", @"Binaries/mmf_test_writeuint.bin", true);
+            using var mmf = new MMFile(@"Binaries/mmf_test_writeuint.bin");
+
+            mmf.WriteUInt(0x15, 0xafbfcfdf);
+
+            Assert.Equal(0xafbfcfdfu, mmf.ReadUInt(0x15));
+        }
+
+        [Fact]
+        public void WriteULong_WritesULong()
+        {
+            File.Copy(@"Binaries/mmf_test.bin", @"Binaries/mmf_test_writeulong.bin", true);
+            using var mmf = new MMFile(@"Binaries/mmf_test_writeulong.bin");
+
+            mmf.WriteULong(0x15, 0xafbfcfdfafbfcfdf);
+
+            Assert.Equal(0xafbfcfdfafbfcfdf, mmf.ReadULong(0x15));
+        }
+
+        [Fact]
+        public void WriteUShort_WritesUShort()
+        {
+            File.Copy(@"Binaries/mmf_test.bin", @"Binaries/mmf_test_writeushort.bin", true);
+            using var mmf = new MMFile(@"Binaries/mmf_test_writeushort.bin");
+
+            mmf.WriteUShort(0x15, 0xafbf);
+
+            Assert.Equal((ushort) 0xafbf, mmf.ReadUShort(0x15));
+        }
+    }
+}
