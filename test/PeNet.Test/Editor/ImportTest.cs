2afed895a394d4b9ed9fd762deccad298fddf940
Noah Potash <noah.potash@outbreaklabs.com>
2021-04-26 19:38:31 -0400

    Added a regression test for an ArgumentOutOfRangeException introduced to AddImport in v2.4.1

--- test/PeNet.Test/Editor/ImportTest.cs
+++ test/PeNet.Test/Editor/ImportTest.cs
@@ -63,5 +63,31 @@ namespace PeNet.Test.Editor
             i.DLL == "ADVAPI32.dll"
             && i.Name == "RegCloseKey"));
         }
+
+        [Fact]
+        public void AddImport_Regression_1()
+        {
+            // This is a regression test against an issue introduced in version 2.4.1.
+
+            //Message: 
+            //    System.ArgumentOutOfRangeException : Specified argument was out of the range of valid values. (Parameter 'length')
+            //  Stack Trace: 
+            //    BufferFile.WriteULong(Int64 offset, UInt64 value) line 51
+            //    ImageThunkData.set_AddressOfData(UInt64 value) line 37
+            //    PeFile.<AddImports>g__AddThunkDatas|1_4(UInt32& offset, List`1 adrList, <>c__DisplayClass1_0& ) line 106
+            //    PeFile.<AddImports>g__AddImportWithNewImpDesc|1_5(UInt32& tmpOffset, Int64& paIdesc, AdditionalImport ai, <>c__DisplayClass1_0& ) line 129
+            //    PeFile.AddImports(List`1 additionalImports) line 148
+            //    ImportTest.AddImport_Regression() line 71
+
+            var peFile = new PeFile(@"Binaries/AddImportsRegression.exe");
+            peFile.AddImports(new List<PeNet.AdditionalImport> { new PeNet.AdditionalImport("test.dll", new List<string> 
+            {
+                "fn0000000140001000","fn0000000140001C50","fn0000000140001C54","fn0000000140001C5C","fn0000000140001C74","fn0000000140001C7C","fn0000000140001C84","fn0000000140001CB4","fn0000000140001CBC","fn0000000140001E18"
+            })});
+
+            Assert.NotNull(peFile.ImportedFunctions.FirstOrDefault(i =>
+           i.DLL == "test.dll"
+           && i.Name == "fn0000000140001000"));
+        }
     }
 }
