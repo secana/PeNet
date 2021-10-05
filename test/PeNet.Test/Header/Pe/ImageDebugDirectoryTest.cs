8457895a8feaf1eefdf7cb0caf7cdfda8cf1bb84
Andre Gleichner <andre.gleichner@gdata.de>
2021-10-05 13:08:32 +0200

    Added test for ExtendedDllCharacteristics and DebugType

--- test/PeNet.Test/Header/Pe/ImageDebugDirectoryTest.cs
+++ test/PeNet.Test/Header/Pe/ImageDebugDirectoryTest.cs
@@ -5,7 +5,7 @@ using Xunit;
 
 namespace PeNet.Test.Header.Pe
 {
-    
+
     public class ImageDebugDirectoryTest
     {
         [Fact]
@@ -13,12 +13,12 @@ namespace PeNet.Test.Header.Pe
         {
             var idd = new ImageDebugDirectory(new BufferFile(RawStructures.RawDebugDirectory), 2);
 
-            Assert.Equal((uint) 0x44332211, idd.Characteristics);
+            Assert.Equal((uint)0x44332211, idd.Characteristics);
             Assert.Equal(0x88776655, idd.TimeDateStamp);
-            Assert.Equal((ushort) 0xaa99, idd.MajorVersion);
-            Assert.Equal((ushort) 0xccbb, idd.MinorVersion);
-            Assert.Equal((uint) 0x11ffeedd, idd.Type);
-            Assert.Equal((uint) 0x55443322, idd.SizeOfData);
+            Assert.Equal((ushort)0xaa99, idd.MajorVersion);
+            Assert.Equal((ushort)0xccbb, idd.MinorVersion);
+            Assert.Equal((uint)0x11ffeedd, idd.Type);
+            Assert.Equal((uint)0x55443322, idd.SizeOfData);
             Assert.Equal(0x99887766, idd.AddressOfRawData);
             Assert.Equal(0xddccbbaa, idd.PointerToRawData);
         }
@@ -35,6 +35,27 @@ namespace PeNet.Test.Header.Pe
             Assert.Equal((uint)0x0000241C, debugEntries[1].AddressOfRawData);
             Assert.Equal((uint)0x00002430, debugEntries[2].AddressOfRawData);
             Assert.Equal((uint)0x00000000, debugEntries[3].AddressOfRawData);
+
+            Assert.Equal(DebugDirectoryType.CodeView, debugEntries[0].DebugType);
+            Assert.Equal(DebugDirectoryType.VcFeature, debugEntries[1].DebugType);
+            Assert.Equal(DebugDirectoryType.Pogo, debugEntries[2].DebugType);
+            Assert.Equal(DebugDirectoryType.Iltcg, debugEntries[3].DebugType);
+        }
+
+        [Fact]
+        public void ParseDebugDirectory_MultipleEntries_ExtendedDllCharacteristics()
+        {
+            var file = @"Binaries/TestCetCompatAndEhCont.exe";
+
+            var debugEntries = new PeFile(file).ImageDebugDirectory;
+
+            Assert.Equal(5, debugEntries.Length);
+            Assert.Equal(DebugDirectoryType.CodeView, debugEntries[0].DebugType);
+            Assert.Equal(DebugDirectoryType.VcFeature, debugEntries[1].DebugType);
+            Assert.Equal(DebugDirectoryType.Pogo, debugEntries[2].DebugType);
+            Assert.Equal(DebugDirectoryType.Iltcg, debugEntries[3].DebugType);
+            Assert.Equal(DebugDirectoryType.ExtendedDllCharacteristics, debugEntries[4].DebugType);
+            Assert.Equal(ExtendedDllCharacteristicsType.CetCompat, debugEntries[4].ExtendedDllCharacteristics);
         }
 
         [Fact]
@@ -47,7 +68,7 @@ namespace PeNet.Test.Header.Pe
 
             Assert.Equal(0x53445352u, cv.CvSignature);
             Assert.Equal(Guid.Parse("0de6dc23-8e19-4bb7-8608-d54b1e6fa379"), cv.Signature);
-            Assert.Equal((ushort) 0x0001, cv.Age);
+            Assert.Equal((ushort)0x0001, cv.Age);
             Assert.Equal("ntkrnlmp.pdb", cv.PdbFileName);
         }
 
